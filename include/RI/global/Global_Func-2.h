97a436dae53ee1a8a83aea94174800fc3bcc52b8
linpz <linpz@mail.ustc.edu.cn>
2022-08-12 18:50:43 +0800

    1. update Global_Func-2

--- include/RI/global/Global_Func-2.h
+++ include/RI/global/Global_Func-2.h
@@ -6,10 +6,44 @@
 #pragma once
 
 #include <complex>
+#include <type_traits>
 
 namespace Global_Func
 {
 	template<typename T> struct To_Real	{ using type=T; };
 	template<typename T> struct To_Real<std::complex<T>> { using type=T; };
 	template<typename T> using To_Real_t = typename Global_Func::To_Real<T>::type;
+	
+	template<typename T> struct To_Complex	{ using type=std::complex<T>; };
+	template<typename T> struct To_Complex<std::complex<T>> { using type=std::complex<T>; };
+	template<typename T> using To_Complex_t = typename To_Complex<T>::type;
+
+
+
+	template<typename> struct is_complex_helper : std::false_type {};
+	template<typename T> struct is_complex_helper<std::complex<T>> : std::true_type {};
+	template<typename T> struct is_complex : is_complex_helper<typename std::remove_const<typename std::remove_reference<T>::type>::type> {};
+
+	// t = convert(t)
+	template<
+		typename Tout, typename Tin,
+		typename std::enable_if<std::is_same<Tin,Tout>::value,int>::type =0>
+	Tout convert(const Tin &t)
+	{ return t; }
+
+	// complex = convert(real)
+	template<
+		typename Tout, typename Tin,
+		typename std::enable_if<!Global_Func::is_complex<Tin >::value,int>::type =0,
+		typename std::enable_if< Global_Func::is_complex<Tout>::value,int>::type =0>
+	Tout convert(const Tin &t)
+	{ return Tout(t,0); }
+
+	// real = convert(complex)
+	template<
+		typename Tout, typename Tin,
+		typename std::enable_if< Global_Func::is_complex<Tin >::value,int>::type =0,
+		typename std::enable_if<!Global_Func::is_complex<Tout>::value,int>::type =0>
+	Tout convert(const Tin &t)
+	{ return t.real(); }
 }
--- include/RI/global/MPI_Wrapper.h
+++ include/RI/global/MPI_Wrapper.h
@@ -7,26 +7,30 @@
 
 #include <mpi.h>
 
+#define MPI_CHECK(x) if((x)!=MPI_SUCCESS)	throw std::runtime_error(std::string(__FILE__)+" line "+std::to_string(__LINE__));
+
 namespace MPI_Wrapper
 {		
-	int mpi_get_rank(const MPI_Comm &mpi_comm)
+	inline int mpi_get_rank(const MPI_Comm &mpi_comm)
 	{
 		int rank_mine;
-		MPI_Comm_rank (mpi_comm, &rank_mine);
+		MPI_CHECK( MPI_Comm_rank (mpi_comm, &rank_mine) );
 		return rank_mine;
 	}
 		
-	int mpi_get_size(const MPI_Comm &mpi_comm)
+	inline int mpi_get_size(const MPI_Comm &mpi_comm)
 	{
 		int rank_size;
-		MPI_Comm_size (mpi_comm, &rank_size);
+		MPI_CHECK( MPI_Comm_size (mpi_comm, &rank_size) );
 		return rank_size;
 	}
 
-	int mpi_get_count(MPI_Status* status, const MPI_Datatype &datatype)
+	inline int mpi_get_count(MPI_Status* status, const MPI_Datatype &datatype)
 	{
 		int count;
-		MPI_Get_count(status, datatype, &count);
+		MPI_CHECK( MPI_Get_count(status, datatype, &count) );
 		return count;
 	}	
 }
+
+#undef MPI_CHECK
