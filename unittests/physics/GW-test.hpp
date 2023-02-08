d7a955b733daa54c53ce0704427d9611829de28f
Minye Zhang <minyez.physchem@gmail.com>
2023-02-08 11:03:57 +0100

    first version G0W0, almost a copy of Exx

---
+++ unittests/physics/GW-test.hpp
@@ -0,0 +1,27 @@
+// ===================
+//  Author: Minye Zhang, almost copied from Exx-test.hpp
+//  date: 2022.06.02
+// ===================
+
+#pragma once
+
+#include "RI/physics/GW.h"
+// #include <complex>
+
+namespace GW_Test
+{
+	template<typename Tdata>
+	void main(int argc, char *argv[])
+	{
+		int mpi_init_provide;
+		MPI_Init_thread(&argc, &argv, MPI_THREAD_MULTIPLE, &mpi_init_provide);
+
+		RI::G0W0<int,int,1,Tdata> g0w0;
+		g0w0.set_parallel(MPI_COMM_WORLD, {{1,{0}},{2,{4}}}, {}, {1});
+		g0w0.set_csm_threshold(1E-4);
+		g0w0.set_Cs({}, 1E-4);
+		g0w0.cal_Sigc({}, 1E-4, {}, 1E-4);
+
+		MPI_Finalize();
+	}
+}
