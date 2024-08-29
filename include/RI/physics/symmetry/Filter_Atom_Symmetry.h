8c2a890a830f003c064a7de7edffa2c2da6141a3
linpz <linpz@mail.ustc.edu.cn>
2024-08-29 17:51:25 +0800

    1. complete all labels in Filter_Atom_Symmetry

--- include/RI/physics/symmetry/Filter_Atom_Symmetry.h
+++ include/RI/physics/symmetry/Filter_Atom_Symmetry.h
@@ -2,6 +2,9 @@
 
 #include "../../ri/Filter_Atom.h"
 #include "Symmetry_Filter.h"
+#include "../../ri/Label_Tools.h"
+
+#include <stdexcept>
 
 namespace RI
 {
@@ -13,61 +16,114 @@ class Filter_Atom_Symmetry: public Filter_Atom<TA, std::pair<TA, TC>>
 	using TAC = std::pair<TA, TC>;
 
 	Filter_Atom_Symmetry(
-		const TC& period,
-		const std::map<std::pair<TA,TA>, std::set<TC>>& irsec)
+		const TC &period,
+		const std::map<std::pair<TA,TA>, std::set<TC>> &irsec)
 		:symmetry(period, irsec){}
 
-	virtual bool filter_for1(const Label::ab_ab& label, const TA& A1) const override
+	virtual bool filter_for1(const Label::ab_ab &label, const TA &A1) const override
 	{
-		switch (label)
+		switch(label)
 		{
-			case Label::ab_ab::a0b0_a2b1: case Label::ab_ab::a0b0_a2b2:
-				return !this->symmetry.is_I_in_irreducible_sector(A1);
+			// a01b01_a2b01:                                      Aa01
+				case Label::ab_ab::a0b0_a2b1:
+				case Label::ab_ab::a0b1_a2b0:
+				case Label::ab_ab::a1b0_a2b1:
+				case Label::ab_ab::a1b1_a2b0:
+			// a01b01_a2b2:                                       Aa01
+				case Label::ab_ab::a0b0_a2b2:
+				case Label::ab_ab::a0b1_a2b2:
+				case Label::ab_ab::a1b0_a2b2:
+				case Label::ab_ab::a1b1_a2b2:
+			// a01b2_a2b01:                                       Aa01
+				case Label::ab_ab::a0b2_a2b0:
+				case Label::ab_ab::a0b2_a2b1:
+				case Label::ab_ab::a1b2_a2b0:
+				case Label::ab_ab::a1b2_a2b1:
+					return !this->symmetry.is_Aa_in_irreducible_sector(A1);
 			default:
-				return false;
+				throw std::invalid_argument("label "+Label_Tools::get_name(label)+" error in "+std::string(__FILE__)+" line "+std::to_string(__LINE__));
 		}
 	}
-	virtual bool filter_for1(const Label::ab_ab& label, const TAC& A1) const override
+	virtual bool filter_for1(const Label::ab_ab &label, const TAC &A1) const override
 	{
-		switch (label)
+		switch(label)
 		{
-			case Label::ab_ab::a0b0_a1b1:
-				return !this->symmetry.is_I_in_irreducible_sector(A1.first);
-			case Label::ab_ab::a0b0_a1b2:
-				return !this->symmetry.is_J_in_irreducible_sector(A1.first);
+			// a01b01_a01b01:                                      Aa2
+				case Label::ab_ab::a0b0_a1b1:
+				case Label::ab_ab::a0b1_a1b0:
+					return !this->symmetry.is_Aa_in_irreducible_sector(A1.first);
+			// a01b01_a01b2:                                       Ab01
+				case Label::ab_ab::a0b0_a1b2:
+				case Label::ab_ab::a0b1_a1b2:
+				case Label::ab_ab::a0b2_a1b0:
+				case Label::ab_ab::a0b2_a1b1:
+					return !this->symmetry.is_Ab_in_irreducible_sector(A1.first);
 			default:
-				return false;
+				throw std::invalid_argument("label "+Label_Tools::get_name(label)+" error in "+std::string(__FILE__)+" line "+std::to_string(__LINE__));
 		}
 	}
 
-	virtual bool filter_for32(const Label::ab_ab& label, const TA& A1, const TAC& A2, const TAC& A3) const override
+	virtual bool filter_for2(const Label::ab_ab &label, const TA &A1, const TAC &A2) const override
 	{
-		switch (label)
+		switch(label)
 		{
-			case Label::ab_ab::a0b0_a2b1: case Label::ab_ab::a0b0_a2b2:
-				return !this->symmetry.in_irreducible_sector(A1, A3);
+			// a01b2_a2b01:                                       Aa01,          Ab01
+				case Label::ab_ab::a0b2_a2b0:
+				case Label::ab_ab::a0b2_a2b1:
+				case Label::ab_ab::a1b2_a2b0:
+				case Label::ab_ab::a1b2_a2b1:
+					return !this->symmetry.in_irreducible_sector(A1, A2);
+			// a01b01_a2b01:                                      Aa01,          Ab01
+			// a01b01_a2b2:                                       Aa01,          Ab2
 			default:
-				return false;
+					return false;
 		}
 	}
-	virtual bool filter_for32(const Label::ab_ab& label, const TAC& A1, const TA& A2, const TAC& A3) const override
+
+	virtual bool filter_for32(const Label::ab_ab &label, const TA &A1, const TAC &A2, const TAC &A3) const override
 	{
-		switch (label)
+		switch(label)
 		{
-			case Label::ab_ab::a0b0_a1b2:
-				return !this->symmetry.in_irreducible_sector(A3, A1);
+			// a01b01_a2b01:                                       Aa01,          Ab01,          Ab2
+				case Label::ab_ab::a0b0_a2b1:
+				case Label::ab_ab::a0b1_a2b0:
+				case Label::ab_ab::a1b0_a2b1:
+				case Label::ab_ab::a1b1_a2b0:
+			// a01b01_a2b2:                                        Aa01,          Ab2,           Ab01
+				case Label::ab_ab::a0b0_a2b2:
+				case Label::ab_ab::a0b1_a2b2:
+				case Label::ab_ab::a1b0_a2b2:
+				case Label::ab_ab::a1b1_a2b2:
+					return !this->symmetry.in_irreducible_sector(A1, A3);
+			// a01b2_a2b01:                                        Aa01,          Ab01,          Aa2
 			default:
-				return false;
+					return false;
 		}
 	}
-	virtual bool filter_for32(const Label::ab_ab& label, const TAC& A1, const TAC& A2, const TAC& A3) const override
+	virtual bool filter_for32(const Label::ab_ab &label, const TAC &A1, const TA &A2, const TAC &A3) const override
 	{
-		switch (label)
+		switch(label)
 		{
-			case Label::ab_ab::a0b0_a1b1:
-				return !this->symmetry.in_irreducible_sector(A1, A3);
+			// a01b01_a01b2:                                        Ab01,         Aa01,          Aa2
+				case Label::ab_ab::a0b0_a1b2:
+				case Label::ab_ab::a0b1_a1b2:
+				case Label::ab_ab::a0b2_a1b0:
+				case Label::ab_ab::a0b2_a1b1:
+					return !this->symmetry.in_irreducible_sector(A3, A1);
 			default:
-				return false;
+				throw std::invalid_argument("label "+Label_Tools::get_name(label)+" error in "+std::string(__FILE__)+" line "+std::to_string(__LINE__));
+		}
+	}
+	virtual bool filter_for32(const Label::ab_ab &label, const TAC &A1, const TAC &A2, const TAC &A3) const override
+	{
+		switch(label)
+		{
+			// a01b01_a01b01:                                       Aa2,           Ab01,          Ab2
+				case Label::ab_ab::a0b0_a1b1:
+				case Label::ab_ab::a0b1_a1b0:
+					return !this->symmetry.in_irreducible_sector(A1, A3);
+			default:
+					throw std::invalid_argument("label "+Label_Tools::get_name(label)+" error in "+std::string(__FILE__)+" line "+std::to_string(__LINE__));
 		}
 	}
 
--- include/RI/physics/symmetry/Symmetry_Filter.h
+++ include/RI/physics/symmetry/Symmetry_Filter.h
@@ -14,9 +14,9 @@ namespace RI
 	class Symmetry_Filter
 	{
 		using TAC = std::pair<TA, TC>;
-		using TIJ = std::pair<TA, TA>;
-		using TIJR = std::pair<TIJ, TC>;
-		using Tsec = std::map<TIJ, std::set<TC>>;
+		using Tab = std::pair<TA, TA>;
+		using TabR = std::pair<Tab, TC>;
+		using Tsec = std::map<Tab, std::set<TC>>;
 
 	  public:
 		Symmetry_Filter(const TC& period_in, const Tsec& irsec)
@@ -25,7 +25,7 @@ namespace RI
 		{
 			NO_SEC_RETURN_TRUE;
 			using namespace Array_Operator;
-			const TIJ& ap = { Aa, Ab.first };
+			const Tab& ap = { Aa, Ab.first };
 			if (irreducible_sector_.find(ap) != irreducible_sector_.end())
 				if (irreducible_sector_.at(ap).find(Ab.second % this->period) != irreducible_sector_.at(ap).end())
 					return true;
@@ -42,27 +42,27 @@ namespace RI
 					return true;
 			return false;
 		}
-		bool is_I_in_irreducible_sector(const TA& Aa) const
+		bool is_Aa_in_irreducible_sector(const TA& Aa) const
 		{
 			NO_SEC_RETURN_TRUE;
 			for (const auto& apRs : irreducible_sector_)
 				if (apRs.first.first == Aa)return true;
 			return false;
 		}
-		bool is_J_in_irreducible_sector(const TA& Ab) const
+		bool is_Ab_in_irreducible_sector(const TA& Ab) const
 		{
 			NO_SEC_RETURN_TRUE;
 			for (const auto& apRs : irreducible_sector_)
 				if (apRs.first.second == Ab)return true;
 			return false;
 		}
-		TIJR get_IJR(const TA& I, const TAC& J) const
+		TabR get_abR(const TA& Aa, const TAC& Ab) const
 		{
-			return { {I,J.first}, J.second % this->period };
+			return { {Aa,Ab.first}, Ab.second % this->period };
 		}
-		TIJR get_IJR(const TAC& I, const TAC& J) const
+		TabR get_abR(const TAC& Aa, const TAC& Ab) const
 		{
-			return { {I.first,J.first}, (J.second - I.second) % this->period };
+			return { {Aa.first,Ab.first}, (Ab.second - Aa.second) % this->period };
 		}
 
 	  public:	// private:
