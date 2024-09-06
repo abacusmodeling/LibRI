bd299faeaa184b912e4bdf5c0e182dfb7f47272a
linpz <linpz@mail.ustc.edu.cn>
2024-09-06 17:42:22 +0800

    1. update version 0.2.1.0

--- include/RI/version.h
+++ include/RI/version.h
@@ -6,7 +6,7 @@
 #pragma once
 
 #define __LIBRI_VERSION_MAJOR		2
-#define __LIBRI_VERSION_MINOR		0
+#define __LIBRI_VERSION_MINOR		1
 #define __LIBRI_VERSION_PATCH		0
 
 #define __LIBRI_VERSION				(  __LIBRI_VERSION_MAJOR * 10000 \
