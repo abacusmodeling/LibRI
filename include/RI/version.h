d54f8311e8988bd0cbad6623025c58908a215174
linpz <linpz@mail.ustc.edu.cn>
2024-09-06 17:07:15 +0800

    1. update version 0.2.0.0

---
+++ include/RI/version.h
@@ -0,0 +1,14 @@
+// ===================
+//  Author: Peize Lin
+//  date: 2024.09.06
+// ===================
+
+#pragma once
+
+#define __LIBRI_VERSION_MAJOR		2
+#define __LIBRI_VERSION_MINOR		0
+#define __LIBRI_VERSION_PATCH		0
+
+#define __LIBRI_VERSION				(  __LIBRI_VERSION_MAJOR * 10000 \
+									 + __LIBRI_VERSION_MINOR * 100 \
+									 + __LIBRI_VERSION_PATCH)
